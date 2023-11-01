7cf03f93b07d4dc3970798b3c6411945613b9133
Aldrei Rigoli <aldreicrigoli@gmail.com>
2023-11-01 00:25:09 -0300

    a

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'piu-3',
+  appName: 'unijui',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
+import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  setupIonicReact
+  IonButton,
+  IonAlert,
+  setupIonicReact,
+  IonContent
 } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -34,42 +27,56 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+let output = document.getElementById('output');
+function fetchData() {
+   fetch('https://api.kanye.rest')
+   .then(response => response.json())
+   .then(data => {
+      output.innerHTML += "Kanye :" + data.quote + "<br/>"; 
+   })
+}
+
+
+
+
+
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
+    <IonContent>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <body>
+   
+    <div class="myDiv">
+        <h1>
+    Bem-vindo ao meu site
+        </h1>
+        <p>Este é um parágrafo de exemplo.
+        </p>
+       
+        <IonButton id="present-alert">Clique Aqui</IonButton>
+      <IonAlert
+        trigger="present-alert"
+        message= "Call Kanye?"
+        buttons={[
+                {
+                  text: "yes",
+                  handler: () =>{fetchData();}
+                }
+        ]}
+      ></IonAlert>
+
+    </div> 
+    <div id = "output"> </div>    
+    <div class="myDiv">
+        <h2>Seção importante</h2>
+        <p>Esta é uma seção com conteúdo relevante.</p>
+    </div>
+    
+    <img src="logo.png"/>
+    </body>
+    </IonContent>
   </IonApp>
 );
 
